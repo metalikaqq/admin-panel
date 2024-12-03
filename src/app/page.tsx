77b129a91270e7a5f77e45428a64a8962ede046d
Sasha Maniak <privetloch@gmail.com>
2024-12-03 21:27:17 +0200

    thm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
 
   return (
     <div className={s.main}>
-      <h1>Welcome to the Home Page!</h1>
+      <h1>Welcome to the Home Page! {some}</h1>
       <p>This is your main content area.</p>
       {/* Add more content as needed */}
     </div>
